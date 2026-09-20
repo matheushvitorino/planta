31532a99fd26ffccdbd61c67a77012e1b146210a
Matheus <matheushenriquevitorino@gmail.com>
2026-09-20 18:02:41 -0300

    feat: Adicionado o PlantaService

---
+++ demo/src/main/java/com/plantas/demo/Plantas/PlantaService.java
@@ -0,0 +1,52 @@
+package com.plantas.demo.Plantas;
+
+import com.plantas.demo.Especies.EspecieModel;
+import com.plantas.demo.Especies.EspecieService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PlantaService {
+    private PlantaRepository plantaRepository;
+    private EspecieService especieService;
+
+    public PlantaService(PlantaRepository plantaRepository, EspecieService especieService) {
+        this.plantaRepository = plantaRepository;
+        this.especieService = especieService;
+    }
+
+    public PlantaModel salvar(PlantaModel planta) {
+        EspecieModel especie = especieService.buscarPorId(
+                planta.getEspecie().getId()
+        );
+
+        planta.setEspecie(especie);
+
+        return plantaRepository.save(planta);
+    }
+    public List<PlantaModel> listar(){
+
+        return  plantaRepository.findAll();
+
+    }
+
+    public PlantaModel buscarPorId(Long id){
+        return plantaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Id nao encontrado"));
+    }
+
+
+    public void deletarPorId(Long id){
+        buscarPorId(id);
+        plantaRepository.deleteById(id);
+    }
+
+    public PlantaModel atualizar(Long id, PlantaModel plantaAtualizada){
+        PlantaModel planta1 = buscarPorId(id);
+
+        planta1.setNome(plantaAtualizada.getNome());
+
+        return plantaRepository.save(planta1);
+    }
+}
