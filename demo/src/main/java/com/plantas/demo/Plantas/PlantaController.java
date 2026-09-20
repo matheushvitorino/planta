0b3749ac0a2468318b7bb6dc530089a1e0a2d2e6
Matheus <matheushenriquevitorino@gmail.com>
2026-09-20 18:01:48 -0300

    feat: Adicionado o PlantaController

---
+++ demo/src/main/java/com/plantas/demo/Plantas/PlantaController.java
@@ -0,0 +1,40 @@
+package com.plantas.demo.Plantas;
+
+import com.plantas.demo.Especies.EspecieModel;
+import com.plantas.demo.Especies.EspecieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("plantas")
+public class PlantaController {
+    @Autowired
+    private PlantaService plantaService;
+
+    @PostMapping
+    public PlantaModel salvar(@RequestBody PlantaModel planta){
+        return plantaService.salvar(planta);
+    }
+
+    @GetMapping("/todos")
+    public List<PlantaModel> listar(){
+        return plantaService.listar();
+    }
+
+    @GetMapping("/{id}")
+    public PlantaModel buscarPorId(@PathVariable Long id){
+        return plantaService.buscarPorId(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id){
+        plantaService.deletarPorId(id);
+    }
+
+    @PutMapping("/{id}")
+    public PlantaModel atualizar(@PathVariable Long id,@RequestBody PlantaModel planta){
+        return plantaService.atualizar(id, planta);
+    }
+}
