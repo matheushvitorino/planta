8b0f4a6fb0745b12aeb0fbd3f823891caf3c39fb
Matheus <matheushenriquevitorino@gmail.com>
2026-09-20 22:19:17 -0300

    Criado a AdubacaoCOntroller

---
+++ demo/src/main/java/com/plantas/demo/Adubacao/AdubacaoController.java
@@ -0,0 +1,34 @@
+package com.plantas.demo.Adubacao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("adubacoes")
+public class AdubacaoController {
+    @Autowired
+    private AdubacaoService adubacaoService;
+
+    @PostMapping
+    public AdubacaoModel salvar(@RequestBody AdubacaoModel adubacaoModel){
+        return  adubacaoService.salvar(adubacaoModel);
+
+    }
+
+    @GetMapping
+    public List<AdubacaoModel> listar(){
+        return adubacaoService.listar();
+    }
+
+    @GetMapping("/{id}")
+    public AdubacaoModel buscarPorId(@PathVariable Long id){
+        return adubacaoService.buscarPorId(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void remover(@PathVariable Long id){
+        adubacaoService.deletarPorId(id);
+    }
+}
