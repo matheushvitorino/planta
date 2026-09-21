4cddbd9847014496d4027f9e2aa6666f7eae936f
Matheus <matheushenriquevitorino@gmail.com>
2026-09-20 22:19:54 -0300

    Criado o AdubacaoRepository

---
+++ demo/src/main/java/com/plantas/demo/Adubacao/AdubacaoRepository.java
@@ -0,0 +1,6 @@
+package com.plantas.demo.Adubacao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdubacaoRepository extends JpaRepository<AdubacaoModel, Long> {
+}
